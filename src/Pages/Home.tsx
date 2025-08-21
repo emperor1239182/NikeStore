8eac48b7ce2744dec8a29894d37d137bf73f5418
Xpression <emmybanks03@gmail.com>
2025-08-21 15:30:40 +0100

    styled nav bar, page components and menu display with responsiveness

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,41 @@
 export const Home = () => {
+
+    const days : string[] = [
+       "Sunday", 
+       "Monday", 
+       "Tuesday", 
+       "Wednessday", 
+       "Thursday", 
+       "Friday", 
+       "Saturday"
+    ]
+    const months : string[] = [
+        "January",
+        "February", 
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ]
+
+    const date = new Date()
+    const day = days[date.getDay()]
+    const dateNum = date.getDate()
+    const month = months[date.getMonth()]
     return (
         <>
-        <h1>Hello</h1>
+        <section className="min-h-screen bg-gray-100 flex">
+        <main  className=" flex-1 max-w-2xl mx-auto p-3 ">
+        <h1 className="font-bold text-[20px] ">Discover</h1>
+        <div className="text-[10px] text-gray-500 font-bold"> {day}, {dateNum} {month}</div>
+        </main>
+        </section>
         </>
     )
 }
--- src/Pages/Layout.tsx
+++ src/Pages/Layout.tsx
@@ -1,41 +1,55 @@
-import { Outlet, Link} from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import { FiHeart, FiHome, FiUser, FiSearch, FiShoppingBag } from "react-icons/fi";
 
 export const Layout = () => {
-    return (
-        <>
-        <section className="flex flex-col min-h-screen">
+  return (
+    <section className="flex min-h-screen">
+      {/* Sidebar / Navbar */}
+      <nav className="menu">
+        <ul className="navListContainer">
+          <li className="navlist hidden sm:block text-xl font-extrabold">
+            <Link to="/home">Nike</Link>
+          </li>
 
-        <nav className="menu">
-        <ul className="flex justify-around">
+          <Link to="/home">
             <li className="navList">
-                <Link to="/home"><FiHome size={18}/></Link>
-                <p >Home</p>
+              <FiHome size={18} />
+              <p className="listText">Home</p>
             </li>
+          </Link>
+
+          <li className="navList">
+            <FiSearch size={18} />
+            <p className="listText">Shop</p>
+          </li>
+
+          <Link to="/favourite">
             <li className="navList">
-                <FiSearch size={18}/> 
-                <p>Shop</p>
+              <FiHeart size={18} />
+              <p className="listText">Favourites</p>
             </li>
+          </Link>
+
+          <Link to="/bag">
             <li className="navList">
-                <Link to="/favourite"><FiHeart size={18}/></Link>
-                <p>Favourites</p>
+              <FiShoppingBag size={18} />
+              <p className="listText">Bag</p>
             </li>
+          </Link>
+
+          <Link to="/login">
             <li className="navList">
-                <Link to="/bag"><FiShoppingBag size={18}/></Link>
-                <p>Bag</p>
+              <FiUser size={18} />
+              <p className="listText">Profile</p>
             </li>
-            <li className="navList">
-                <Link to="/login"><FiUser size={18}/></Link>
-                <p>Profile</p>
-            </li>
+          </Link>
         </ul>
-        </nav>
-        
-        <main className="flex-1">
-        <Outlet />
-        </main>
+      </nav>
 
-        </section>
-        </>
-    )
-}
+      {/* Main Page Content */}
+      <main className="flex-1 max-w-4xl mx-auto p-4 sm:ml-[25%]">
+        <Outlet />
+      </main>
+    </section>
+  );
+};
